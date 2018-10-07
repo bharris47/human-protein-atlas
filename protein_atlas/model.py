38b320679f42c1162005c69455f09e68687fce37
Ben Harris <benharris247@gmail.com>
2018-10-06 20:50:19 -0700

    add more dropout

--- protein_atlas/model.py
+++ protein_atlas/model.py
@@ -18,13 +18,17 @@ def simple_convnet(input_shape, n_classes, base_filters, activation, fc_size, dr
     conv_1 = Conv2D(base_filters, 7, activation=activation)(image)
     conv_1 = BatchNormalization()(conv_1)
     conv_2 = _conv_block(conv_1, filters=base_filters, kernel_size=3, activation=activation, pool=2)
+    conv_2 = Dropout(dropout)(conv_2)
     conv_3 = _conv_block(conv_2, base_filters * 2, kernel_size=3, activation=activation)
     conv_4 = _conv_block(conv_3, base_filters * 2, kernel_size=3, activation=activation, pool=2)
+    conv_4 = Dropout(dropout)(conv_4)
     conv_5 = _conv_block(conv_4, base_filters * 4, kernel_size=3, activation=activation)
     conv_6 = _conv_block(conv_5, base_filters * 4, kernel_size=3, activation=activation, pool=2)
+    conv_6 = Dropout(dropout)(conv_6)
     conv_7 = _conv_block(conv_6, base_filters * 2, kernel_size=1, activation=activation)
 
     hidden = GlobalMaxPooling2D()(conv_7)
+    hidden = Dropout(dropout)(hidden)
 
     fc_1 = Dense(fc_size, activation=activation)(hidden)
     fc_1 = Dropout(dropout)(fc_1)
